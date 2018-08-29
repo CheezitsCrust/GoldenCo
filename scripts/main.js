42421fb60e93616d8e1f89ee46f95527f67a9c93
Yelsin S <YelsinSepulveda@gmail.com>
2018-08-29 04:24:57 -0400

    More updates to UI
    Created pull out navigation menu.
    Added new fonts from google fonts.
    Placed social media links inside navigation menu.
    
    For the most part the home page is done. Will begin work on next page, About Us

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,11 @@ function scrollBttnClick() {
 var target = document.getElementById("secondSection");
 zenscroll.to(target);
 }
+function openNav() {
+    document.getElementById("navBar").style.width = "300px";
+    document.getElementById("Mtext").style.visibility = "hidden";
+}
+function closeNav() {
+    document.getElementById("navBar").style.width = "0";
+    document.getElementById("Mtext").style.visibility = "visible";
+}
