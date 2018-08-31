b0c937c24945fbbe047524d9f90f24499c90909b
Yelsin S <YelsinSepulveda@gmail.com>
2018-08-31 09:50:39 -0400

    Created about is section and added anchors

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,31 @@
+var vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+document.getElementById('topContainer').setAttribute("style","width:vw");
+document.getElementById('topContainer').style.width='vw';
+
 function scrollBttnClick() {
-var target = document.getElementById("secondSection");
+var target = document.getElementById("smallGreeting");
 zenscroll.to(target);
 }
+
 function openNav() {
-    document.getElementById("navBar").style.width = "300px";
+    document.getElementById("navBar").style.width = "330px";
     document.getElementById("Mtext").style.visibility = "hidden";
 }
+
 function closeNav() {
     document.getElementById("navBar").style.width = "0";
     document.getElementById("Mtext").style.visibility = "visible";
+}
+
+function jumpShop() {
+  var jumpS = document.getElementById("smallGreeting");
+  zenscroll.to(jumpS);
+}
+
+
+function jumpGallery(){
+  var jumpG = document.getElementById("navBar");
+  zenscroll.to(jumpG);
 }
